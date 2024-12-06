ecf9c9c97d38a1de3760a47e96ddffc8b5eda153
Sanchezz99 <stupin.2006@outlook.com>
2024-12-06 11:02:23 +0300

    ПЗ 2 выполнена

--- Pz_2/PZ_2_1.py
+++ Pz_2/PZ_2_1.py
@@ -1,8 +1,8 @@
-def day_of_week(k):
-    day_number = k % 7
+def day_of_week(K):
+    day_number = K % 7
     return day_number
-k = int(input("Введите номер дня года (1-365): "))
-if 1 <= k <= 365:
-    print(f"Номер дня недели для {k}-го дня года: {day_of_week(k)}")
+K = int(input("Введите номер дня года (1-365): "))
+if 1 <= K <= 365:
+    print(f"Номер дня недели для {K}-го дня года: {day_of_week(K)}")
 else:
     print("Увы, вы ошиблись. Введите число в диапазоне от 1 до 365.")
