64f5a8e9cdbaaa9924ddca0af8bd0c5aa133aa8b
Sanchezz99 <stupin.2006@outlook.com>
2024-12-07 15:52:53 +0300

    Предварительно PZ_3 готова

--- PZ_3/PZ_3_1.py
+++ PZ_3/PZ_3_1.py
@@ -1,1 +1,10 @@
-print("Hi")
+def coordinates_rectangle(x, y, x1, y1, x2, y2):
+    return (x1 < x < x2) and (y1 < y < y2)
+x = 4
+x1 = 3
+x2 = 5
+y = 8
+y1 = 7
+y2 = 9
+result = coordinates_rectangle(x, y, x1, y1, x2, y2)
+print(result)
--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -1,1 +1,22 @@
-print("Hi")
+def numeric_axis(A, B, C):
+   if A > B:
+       distance_B = A - B
+   else:
+       distance_B = B - A
+   if A > C:
+       distance_C = A - C
+   else:
+       distance_C = C - A
+   if distance_B < distance_C:
+       axis_point = "B"
+       main_distance = distance_B
+   else:
+       axis_point = "С"
+       main_distance = distance_C
+   return axis_point, main_distance
+A = 6
+B = 8
+C = 7
+axis_result = numeric_axis(A, B, C)
+print(f"Самая близкая точка к A: {axis_result[0]}")
+print(f"Расстояние от A: {axis_result[1]}")
