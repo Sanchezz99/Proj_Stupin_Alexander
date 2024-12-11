1b2dc9818037c11b3e2f9efa38a1809c12d69b8b
Sanchezz99 <stupin.2006@outlook.com>
2024-12-11 15:11:51 +0300

    PZ_4_1 немного поменял

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,18 +1,18 @@
 #Дано целое число N (>0). Найти произведение 1.1 • 1.2 • 1.3 •... (N сомножителей).
-def product_number(n):
+def product_number(N):
     try:
-        if n < 0:
-            raise ValueError("n не должен быть отрицательным")
+        if N < 0:
+            raise ValueError("N не должен быть отрицательным")
         product = 1.0
         i = 1
-        while i <= n:
+        while i <= N:
             product *= 1 + i / 10
             i += 1
         return product
     except ValueError as e:
         print(f"Увы, произошла ошибка: {e}")
         return 0
-n = int(input("Введите число n: "))
-result = product_number(n)
+N = int(input("Введите число N: "))
+result = product_number(N)
 if result != 0:
     print(result)
