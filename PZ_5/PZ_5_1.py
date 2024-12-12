440118947d7cf3df63cbfa2a3e5fa2409cf6ebd3
Sanchezz99 <stupin.2006@outlook.com>
2024-12-12 17:29:06 +0300

    PZ_5_1 Немноого изменена

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,8 +1,9 @@
+#Составить функцию, которая напечатает сорок любых символов.
 def forty_characters():
     characters = "QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,./`1234567890-=!@#$%^&*()_+<>?:"
     while True:
         try:
-            num_characters = int(input("Введите количество символов для вывода (мвусимум 40): "))
+            num_characters = int(input("Введите количество символов для вывода (максимум 40): "))
             if num_characters < 1 or num_characters > 40:
                 raise ValueError("Число должно быть от 1 до 40")
             print(characters[:num_characters])
