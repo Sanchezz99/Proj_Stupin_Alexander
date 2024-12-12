f5a398d77a65bdd3d0721325c6c45aed0f0f7b9e
Sanchezz99 <stupin.2006@outlook.com>
2024-12-12 17:22:29 +0300

    Немного поменял PZ_4 PZ_5

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -1,4 +1,9 @@
-def swap(X, Y):
+#Описать функцию Swap(X, Y), меняющую содержимое переменных X и Y 
+#(X и Y — вещественные параметры, являющиеся одновременно входными и выходными). 
+#С ее помощью для данных переменных A, B, C, D последовательно 
+#поменять содержимое следующих пар: 
+#A и B, C и D, B и C и вывести новые значения A, B, C, D.
+def Swap(X, Y):
     return Y, X
 def function_input(swapping):
     while True:
@@ -11,7 +16,7 @@ A = function_input("Введите значение A: ")
 B = function_input("Введите значение B: ")  
 C = function_input("Введите значение C: ") 
 D = function_input("Введите значение D: ")  
-A, B = swap(A, B)
-C, D = swap(C, D)
-B, C = swap(B, C)
+A, B = Swap(A, B)
+C, D = Swap(C, D)
+B, C = Swap(B, C)
 print(f"Новые значения для переменных: A = {A}, B = {B}, C = {C}, D = {D}")
