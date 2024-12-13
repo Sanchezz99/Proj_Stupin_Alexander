9b31ab4a7e7fe8aa677fcdce8c3307cd489f0500
Sanchezz99 <stupin.2006@outlook.com>
2024-12-13 14:16:44 +0300

    PZ_5 Переделана и готова

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,13 +1,10 @@
 #Составить функцию, которая напечатает сорок любых символов.
-def forty_characters():
-    characters = "QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,./`1234567890-=!@#$%^&*()_+<>?:"
-    while True:
-        try:
-            num_characters = int(input("Введите количество символов для вывода (максимум 40): "))
-            if num_characters < 1 or num_characters > 40:
-                raise ValueError("Число должно быть от 1 до 40")
-            print(characters[:num_characters])
-            break
-        except ValueError as e:
-            print(f"Увы, произошла ошибка: {e}. Попробуйте снова.")
-forty_characters()
+import random
+def generate_random_characters(length=40):
+    random_characters = ""
+    while len(random_characters) < length:
+        random_char = chr(random.randint(32, 126))
+        random_characters += random_char
+    return random_characters
+random_characters = generate_random_characters()
+print(random_characters)
--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -3,7 +3,7 @@
 #С ее помощью для данных переменных A, B, C, D последовательно 
 #поменять содержимое следующих пар: 
 #A и B, C и D, B и C и вывести новые значения A, B, C, D.
-def Swap(X, Y):
+def swap(X, Y):
     return Y, X
 def function_input(swapping):
     while True:
@@ -11,12 +11,12 @@ def function_input(swapping):
             value = float(input(swapping))
             return value
         except ValueError:
-            print("Увы, произошла ошибка. Введите число")
+            print("Увы, произошла ошибка. Пожалуйста, введите действительное число.")
 A = function_input("Введите значение A: ")
 B = function_input("Введите значение B: ")  
 C = function_input("Введите значение C: ") 
 D = function_input("Введите значение D: ")  
-A, B = Swap(A, B)
-C, D = Swap(C, D)
-B, C = Swap(B, C)
+A, B = swap(A, B)
+C, D = swap(C, D)
+B, C = swap(B, C)
 print(f"Новые значения для переменных: A = {A}, B = {B}, C = {C}, D = {D}")
