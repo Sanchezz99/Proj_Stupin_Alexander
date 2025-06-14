79a3cf09772c1d77fefadd66ad04f0ddb56a5a44
Sanchezz99 <stupin.2006@outlook.com>
2025-06-14 10:07:38 +0300

    изменил pz_16

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -1,22 +1,38 @@
-#Создайте базовый класс "Животное" со свойствами "вид", "количество лап", "цвет 
-#шерсти". От этого класса унаследуйте класс "Собака" и добавьте в него свойства 
-#"кличка" и "порода".  
-class Animal:
-    def __init__(self, type_anim, kol_legs, color):
-        self.type_anim = type_anim
-        self.kol_legs = kol_legs
-        self.color = color
-    def __str__(self):
-        return f"Вид: {self.type_anim}; Количество лап: {self.kol_legs}; Цвет шерсти: {self.color}."
-class Dog(Animal):
-    def __init__(self, type_anim, kol_legs, color, name, breed):
-        super().__init__(type_anim, kol_legs, color)
-        self.name = name
-        self.breed = breed
-    def __str__(self):
-        return (f"Вид: {self.type_anim}; Количество лап: {self.kol_legs}; Цвет шерсти: {self.color}; "
-                f"Кличка: {self.name}; Порода: {self.breed}.")
-animal = Animal("Хомяк", 4, "Белый")
-print(animal)
-dog = Dog("Собака", 4, "Белый", "Принц", "Хаски")
-print(dog)
+#Создайте класс «Календарь», который имеет атрибуты год, месяц и день. Добавьте 
+#методы для определения дня недели, проверки на високосный год и определения 
+#количества дней в месяце.
+import datetime 
+class Kalendar:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.rusday = {
+            "Monday": "Понедельник",
+            "Tuesday": "Вторник",
+            "Wednesday": "Среда",
+            "Thursday": "Четверг",
+            "Friday": "Пятница",
+            "Saturday": "Суббота",
+            "Sunday": "Воскресенье"
+        }
+    def visok_year(self):
+        return (self.year % 400 == 0) or (self.year % 4 == 0 and self.year % 100 != 0)
+    def kolvo_day(self):
+        if self.month == 2:
+            return 29 if self.visok_year() else 28
+        elif self.month in [4, 6, 9, 11]:
+            return 30
+        else: 
+            return 31
+    def day_of_week(self):
+        date = datetime.date(self.year, self.month, self.day)
+        engday = date.strftime("%A")
+        return self.rusday[engday]
+    def main_information(self):
+        print(f"Дата: {self.day}, {self.month}, {self.year}")
+        print("Нынешний год високосный" if self.visok_year() else "Нынешний год не високосный")
+        print(f"В этом месяце {self.kolvo_day()} дней")
+        print(f"День недели: {self.day_of_week()}")
+information = Kalendar(2025, 1, 6)
+information.main_information()
--- PZ_16/PZ_16_2.py
+++ PZ_16/PZ_16_2.py
@@ -1,23 +1,22 @@
-#Создайте класс «Календарь», который имеет атрибуты год, месяц и день. Добавьте 
-#методы для определения дня недели, проверки на високосный год и определения 
-#количества дней в месяце. 
-class Kalendar:
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-    def visok_year(self):
-        return self.year % 4
-    def kolvo_day(self):
-        if self.month == 2:
-            return 29 if self.visok_year else 28
-        elif self.month in [4, 6, 9, 11]:
-            return 30
-        else: 
-            return 31
-    def main_information(self):
-        print(f"Дата: {self.day}, {self.month}? {self.year}")
-        print("Нынешний год високосный" if self.visok_year() else "Нынешний год не високосный")
-        print(f"В этом месяце {self.month} дней")
-information = Kalendar(2025, 11, 6)
-information.main_information()
+#Создайте базовый класс "Животное" со свойствами "вид", "количество лап", "цвет 
+#шерсти". От этого класса унаследуйте класс "Собака" и добавьте в него свойства 
+#"кличка" и "порода".  
+class Animal:
+    def __init__(self, type_anim, kol_legs, color):
+        self.type_anim = type_anim
+        self.kol_legs = kol_legs
+        self.color = color
+    def __str__(self):
+        return f"Вид: {self.type_anim}; Количество лап: {self.kol_legs}; Цвет шерсти: {self.color}."
+class Dog(Animal):
+    def __init__(self, type_anim, kol_legs, color, name, breed):
+        super().__init__(type_anim, kol_legs, color)
+        self.name = name
+        self.breed = breed
+    def __str__(self):
+        return (f"Вид: {self.type_anim}; Количество лап: {self.kol_legs}; Цвет шерсти: {self.color}; "
+                f"Кличка: {self.name}; Порода: {self.breed}.")
+animal = Animal("Хомяк", 4, "Белый")
+print(animal)
+dog = Dog("Собака", 4, "Белый", "Принц", "Хаски")
+print(dog)
